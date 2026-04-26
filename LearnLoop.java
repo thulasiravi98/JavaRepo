65810e1dd87111a3945bec03bb3efe2ea771682d
thulasiravi98 <90749141+thulasiravi98@users.noreply.github.com>
2026-04-26 12:37:46 +0530

    Add files via upload

---
+++ LearnLoop.java
@@ -0,0 +1,20 @@
+package org.testleaf.week1.day2;
+
+public class LearnLoop {
+
+    public static void main(String[] args) {
+        int maxRange = 10;
+        for(int i=1; i<=maxRange; i++){
+            if(i%2!=0)
+                {
+
+            System.out.println(i);
+            }
+           
+        
+       
+
+        }
+    }
+
+}
