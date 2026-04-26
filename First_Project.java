5b380deb6d811a82d0599ed98014930e7912ba18
thulasiravi98 <90749141+thulasiravi98@users.noreply.github.com>
2026-04-26 10:17:49 +0530

    Add files via upload

---
+++ First_Project.java
@@ -0,0 +1,7 @@
+package org.testleaf.week1.day1;
+
+public class First_Project {
+public static void main(String[] args) {
+    System.out.println("Hello World");
+}
+}
---
+++ Learning_Project.java
@@ -0,0 +1,14 @@
+package org.testleaf;
+
+public class Learning_Project {
+public static void main(String[] args) {
+    char browserLogo='f';
+    int releaseYear=1998;
+    boolean isVisible=true;
+    String broswerName="firefox";
+    float browserVersion=100.2f;
+
+    System.out.println(broswerName+" "+releaseYear);
+
+}
+}
